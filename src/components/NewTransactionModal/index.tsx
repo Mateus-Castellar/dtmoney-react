42b4ee5d9d64fcbb4c3e81457dbd199c3fa76fbc
Mateus Castellar <mateuscastellar@outlook.com>
2023-02-13 14:32:12 -0300

    style: Customizacao modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
+import closeImg from "../../assets/close.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +12,28 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal onRequestClose={onRequestClose} isOpen={isOpen}>
-      <h2>Cadastrar Transação</h2>
+    <Modal
+      onRequestClose={onRequestClose}
+      isOpen={isOpen}
+      overlayClassName="react-modal-overley"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="fechar modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input placeholder="título" />
+        <input placeholder="valor" type="number" />
+        <input placeholder="categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
