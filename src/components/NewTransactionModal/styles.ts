bb4fcd5e854fa965362dc1f5a887284af8f4d394
Mateus Castellar <mateuscastellar@outlook.com>
2023-02-13 15:24:28 -0300

    feat: Alterando estilo de acordo com condicao em js

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -50,32 +50,38 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-    background-color: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+interface RadioBoxProps {
+  isActive: boolean;
+}
 
-    transition: border-color 0.4s;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    &:hover {
-      border-color: ${darken(0.1, "#d7d7d7")};
-    }
+  background-color: ${(props) => (props.isActive ? "#c3c3c3" : "#transparent")};
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    span {
-      display: inline-block;
-      font-size: 1rem;
-      margin-left: 1rem;
-      color: var(--text-title);
-    }
+  transition: border-color 0.4s;
+
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    font-size: 1rem;
+    margin-left: 1rem;
+    color: var(--text-title);
   }
 `;
