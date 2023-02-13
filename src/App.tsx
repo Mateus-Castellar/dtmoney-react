ae56746fc2a1c181e03a81a4b86ea4765009cac1
Mateus Castellar <mateuscastellar@outlook.com>
2023-02-13 10:47:34 -0300

    feat: Modal com react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
+import { useState } from "react";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
-      <Dashboard />
       <GlobalStyle />
+
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <Modal
+        onRequestClose={handleCloseNewTransactionModal}
+        isOpen={isNewTransactionModalOpen}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
+
+      <Dashboard />
     </>
   );
 }
