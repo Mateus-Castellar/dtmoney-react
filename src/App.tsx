9a9679d5a2b69e7715a5007c7bb182710cc76e63
Mateus Castellar <mateuscastellar@outlook.com>
2023-02-14 10:10:52 -0300

    feat: Contexto para armazenar transacoes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { GlobalStyle } from "./styles/global";
 import Modal from "react-modal";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import {
+  TransactionsContext,
+  TransactionsProvider,
+} from "./TransactionsContext";
 
 Modal.setAppElement("#root");
 
@@ -20,7 +24,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <GlobalStyle />
 
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
@@ -31,6 +35,6 @@ export function App() {
       />
 
       <Dashboard />
-    </>
+    </TransactionsProvider>
   );
 }
